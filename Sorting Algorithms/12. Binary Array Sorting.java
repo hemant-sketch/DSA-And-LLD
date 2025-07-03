c81f6a506be222af536a5c1296f56c973d131417
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-03 16:39:18 +0530

    Create 12. Binary Array Sorting.java

---
+++ Sorting Algorithms/12. Binary Array Sorting.java
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/binary-array-sorting-1587115620/1
+
+// TC = O(N)
+// SC = O(1)
+
+class Solution {
+    public void swap(int[] nums, int idx1, int idx2){
+        int temp = nums[idx1];
+        nums[idx1] = nums[idx2];
+        nums[idx2] = temp;
+    }
+    // Function to sort the binary array in non-decreasing order
+    public void binSort(int[] nums) {
+        // code here
+        int left = 0;
+        int right = 0;
+
+        while(right < nums.length){
+            if(nums[right] < 1){
+                swap(nums, left, right);
+                left++;
+                right++;
+            }else{
+                right++;
+            }
+        }
+        
+    }
+}
