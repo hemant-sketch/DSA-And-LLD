3387ff11e7faaf1d3f4edce611e33c4a1b318636
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-04 15:10:42 +0530

    Create TwoSumII.java

---
+++ Two Pointer and Sliding Window/TwoSumII.java
@@ -0,0 +1,28 @@
+// Lc 167
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+
+// TC = O(N)
+// SC = O(1)
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        int[] ans = new int[2]; 
+
+        while(left < right){
+            if(nums[left] + nums[right] == target){
+                ans[0] = left + 1;
+                ans[1] = right + 1;
+                break;
+            }
+
+            if(nums[left] + nums[right] > target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+        return ans;
+    }
+}
