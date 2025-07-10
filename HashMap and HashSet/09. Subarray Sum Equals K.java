cc6aa097f6df85cf168286639ad1135b228b3fb4
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-10 16:04:43 +0530

    Create 09. Subarray Sum Equals K.java

---
+++ HashMap and HashSet/09. Subarray Sum Equals K.java
@@ -0,0 +1,38 @@
+// Lc 560
+// https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+// Brute force possible TLE
+// TC = O(N^2)
+// TC = O(1)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            int sum = 0;
+            for(int j = i; j < nums.length; j++){
+                sum += nums[j];
+                if(sum == k) count++;
+            }
+        }
+        return count;
+    }
+}
+
+// HashMap (Optimised)
+// TC = O(N)
+// TC = O(N)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        freq.put(0, 1);   // very important line
+        int sum = 0;
+        int count = 0;
+        
+        for(int val : nums){
+            sum += val;
+            count += freq.getOrDefault(sum - k, 0);
+            freq.put(sum, freq.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
