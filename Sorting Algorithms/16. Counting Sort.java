8003c361d88f88b673914037da18f2ddb6428276
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-14 17:11:26 +0530

    Create 16. Counting Sort.java

---
+++ Sorting Algorithms/16. Counting Sort.java
@@ -0,0 +1,24 @@
+// TC = O(N+D)    // D is the size of an array
+// SC = O(N)
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int min = -50000;
+        int max = 50000;
+        int[] freq = new int [max - min + 1];
+
+        for(int ele : nums){
+            freq[ele - min]++;
+        }
+
+        int idx = 0;
+        for(int ele = min; ele < max; ele++){
+            while(freq[ele - min] > 0){
+                nums[idx++] = ele;
+                freq[ele - min]--;
+            }
+        }   
+        return nums;
+    }
+
+}
