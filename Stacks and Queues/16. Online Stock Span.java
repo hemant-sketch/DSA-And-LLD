d0113b702045d6c3c37894d8d3ff7063cd6bd7f3
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-16 19:50:03 +0530

    Update 16. Online Stock Span.java

--- Stacks and Queues/16. Online Stock Span.java
+++ Stacks and Queues/16. Online Stock Span.java
@@ -4,8 +4,8 @@
 // TC = O(N)
 // SC = O(N)
 
-class StockSpanner {
-    Stack<Integer> stk = new Stack<>();
+class StockSpanner { 
+    Stack<Integer> stk = new Stack<>();    // remember udhr stack of index bnra tah idhr stack of span bnra hai
     ArrayList<Integer> arr = new ArrayList<>();
 
     public int next(int price) {
@@ -15,7 +15,7 @@ class StockSpanner {
 
         int span = (stk.size() > 0) ? arr.size() - stk.peek() : arr.size() + 1;
 
-        stk.push(arr.size());
+        stk.push(arr.size());     // stack mein days aur arr mein prices
         arr.add(price);
         return span;
         
