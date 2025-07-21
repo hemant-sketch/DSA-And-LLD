cbb7c25c79d8d71622946f9a7222bc8494df615c
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-21 21:30:52 +0530

    Create 23. Remove K Digits.java

---
+++ Stacks and Queues/23. Remove K Digits.java
@@ -0,0 +1,33 @@
+// Lc 402
+// https://leetcode.com/problems/remove-k-digits/description/
+
+// TC = O(1)
+// SC = O(N) because of string builder
+
+// eg 2560079489
+
+class Solution {
+    public String removeKdigits(String nums, int k) {
+        StringBuilder res = new StringBuilder();
+
+        char[] chs = nums.toCharArray();
+        for(char ch : chs){
+            while(k > 0 && res.length() > 0 && res.charAt(res.length()-1) > ch){
+                res.deleteCharAt(res.length()-1);
+                k--;
+            } 
+            res.append(ch);
+        }
+
+        while(k-- > 0){
+            res.deleteCharAt(res.length()-1);
+        }
+
+        int idx = 0;
+        while(idx < res.length() && res.charAt(idx) == '0'){
+            idx++;
+        }
+        if(idx == res.length()) return "0";
+        return res.toString().substring(idx);
+    }
+}
