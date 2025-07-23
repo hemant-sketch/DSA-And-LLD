c5c4ac52373b977f62e38c0f3c8f84df7eadd9c5
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-23 21:09:20 +0530

    Create 24. Remove Duplicate Letters.java

---
+++ Stacks and Queues/24. Remove Duplicate Letters.java
@@ -0,0 +1,33 @@
+// Lc 316
+// https://leetcode.com/problems/remove-duplicate-letters/description/
+
+// TC = O(N)
+// SC = O(26) O(1) 
+
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] freq = new int[26];
+        boolean[] present = new boolean[26];
+
+        char[] chs = s.toCharArray();
+        for(char ch : chs) freq[ch - 'a']++;
+
+        StringBuilder res = new StringBuilder();
+
+        for(char ch : chs){
+            freq[ch-'a']--;
+            if(present[ch-'a'] == true) continue;
+            present[ch-'a'] = true;
+
+            while(res.length() > 0){
+                int top = res.charAt(res.length()-1);
+                if(top > ch && freq[top-'a'] > 0){
+                    res.deleteCharAt(res.length()-1);
+                    present[top-'a'] = false;
+                }else break;
+            }
+            res.append(ch);
+        }
+        return res.toString();
+    }
+}
