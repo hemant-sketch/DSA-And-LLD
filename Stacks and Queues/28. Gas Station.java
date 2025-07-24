9862139299b4400eb57ca1bff0036e2f644fb1e7
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-24 21:01:26 +0530

    Create 28. Gas Station.java

---
+++ Stacks and Queues/28. Gas Station.java
@@ -0,0 +1,53 @@
+// Lc 134
+// https://leetcode.com/problems/gas-station/description/
+
+// TC = O(N^2)
+// SC = O(1)
+// TLE will give
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        for(int st = 0; st < n; st++){
+            int fuel = 0;
+            boolean ans = true;
+
+            for(int jump = 0; jump < n; jump++){
+                int curr = (st + jump) % n;
+                fuel += gas[curr];
+
+                if(fuel < cost[curr]){
+                    ans = false;
+                    break;
+                }
+                fuel -= cost[curr];
+            }  
+            if(ans == true) return st;
+        }
+        return -1;
+    }
+}
+
+// TC = O(N)
+// SC = O(N)
+// optimised 
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int deficit = 0, fuel = 0, start = 0;
+
+        for(int idx = 0; idx < gas.length; idx++){
+            fuel += gas[idx] - cost[idx];
+
+            if(fuel < 0){
+                start = idx + 1;
+                deficit += -fuel;
+                fuel = 0;
+            }
+        }
+
+        if(fuel >= deficit) return start;
+        return -1;
+    }
+}
+
