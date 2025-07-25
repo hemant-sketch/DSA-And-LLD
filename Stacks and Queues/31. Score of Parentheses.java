6eedd7d51f1fc943f3c0a21de2a44110dd8a4f0c
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-25 16:52:29 +0530

    Update 31. Score of Parentheses.java

--- Stacks and Queues/31. Score of Parentheses.java
+++ Stacks and Queues/31. Score of Parentheses.java
@@ -1,6 +1,35 @@
 // Lc 856
 // https://leetcode.com/problems/score-of-parentheses/description/
 
-// TC = O()
-// SC = O(0
+// TC = O(N)
+// SC = O(N)
 
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stk = new Stack<>();
+
+        for(int idx = 0; idx < s.length(); idx++){
+            char ch = s.charAt(idx);
+
+            if(ch == '('){
+                stk.push(-1);
+            }else{
+
+                int ans = 0;
+                while(stk.peek() != -1){
+                    ans += stk.pop();
+                }
+                stk.pop();
+
+                if(ans == 0) stk.push(1);
+                else stk.push(2 * ans);
+            }
+        }
+        int ans = 0;
+        while(stk.size() > 0){
+            ans += stk.pop();
+        }
+
+        return ans;
+    }
+}
