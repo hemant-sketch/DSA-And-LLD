968d0ee7d55bb8ff4c070125db296b6c7c68f789
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-28 13:00:33 +0530

    Create 02. Binary Tree Postorder Traversal.java

---
+++ Trees/02. Binary Tree Postorder Traversal.java
@@ -0,0 +1,21 @@
+// Lc 145
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+// TC = O(N)
+// SC = O(N)
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        postorder(root, ans);
+        return ans;
+    }
+    public void postorder(TreeNode root, List<Integer> ans){
+        if(root == null){
+            return;
+        }
+        postorder(root.left, ans);
+        postorder(root.right, ans);
+        ans.add(root.val);
+    }
+}
