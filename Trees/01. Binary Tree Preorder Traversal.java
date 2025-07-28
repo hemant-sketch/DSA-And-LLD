0d44d3be859a58f117b18e18492d1f4f6042807f
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-28 12:58:13 +0530

    Create 01. Binary Tree Preorder Traversal.java

---
+++ Trees/01. Binary Tree Preorder Traversal.java
@@ -0,0 +1,21 @@
+// Lc 144
+// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+// TC = O(N)
+// SC = O(N)
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List <Integer> ans = new ArrayList <>();
+        preorder (root, ans);
+        return ans;
+    }
+    public void preorder (TreeNode root, List <Integer> ans){
+        if(root == null){
+            return;
+        }
+        ans.add(root.val);
+        preorder(root.left, ans);
+        preorder(root.right, ans);
+    }
+}
