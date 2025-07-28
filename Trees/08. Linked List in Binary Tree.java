6c792e8c63b6745db979411ecb9cc42fc61e6f3e
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-28 16:20:42 +0530

    Create 08. Linked List in Binary Tree.java

---
+++ Trees/08. Linked List in Binary Tree.java
@@ -0,0 +1,21 @@
+// Lc 1367
+// https://leetcode.com/problems/linked-list-in-binary-tree/description/
+
+// TC = O(N)
+// SC = O(H)
+
+class Solution {
+    public boolean dfs(ListNode head, TreeNode root){
+        if(head == null) return true;
+        if(root == null) return  false;
+        if(root.val != head.val) return false;
+
+        return dfs(head.next, root.left) || dfs(head.next, root.right);
+    }
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(head == null) return true;
+        if(root == null) return  false;
+        
+        return dfs(head, root) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+}
