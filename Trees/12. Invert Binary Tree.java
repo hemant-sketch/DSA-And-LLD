090189ec744c7d8571721326ee679f1c885d850b
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-29 11:48:37 +0530

    Update 12. Invert Binary Tree.java

--- Trees/12. Invert Binary Tree.java
+++ Trees/12. Invert Binary Tree.java
@@ -4,6 +4,7 @@
 // TC = O(N)
 // TC = O(H)
 
+// preorder
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root == null){
@@ -15,5 +16,21 @@ class Solution {
         root.right = l;
         return root;
 
+    }
+}
+
+// postorder
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
