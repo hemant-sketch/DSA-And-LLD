00c667f917ce3a324385a6cf238cb94a219892f1
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-07-29 11:57:09 +0530

    Create 14. Subtree of Another Tree.java

---
+++ Trees/14. Subtree of Another Tree.java
@@ -0,0 +1,20 @@
+// Lc 572
+// https://leetcode.com/problems/subtree-of-another-tree/description/
+
+// TC = O(N)
+// SC = O(H)
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;  
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    } 
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null) return true;  
+        if(root == null || subRoot == null) return false;
+
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+}
