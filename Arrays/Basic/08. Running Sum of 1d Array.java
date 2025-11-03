c8b87fb64fe1d0aa503bb6c4418233459523d89c
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-11-03 15:08:53 +0530

    Create 08. Running Sum of 1d Array.java

---
+++ Arrays/Basic/08. Running Sum of 1d Array.java
@@ -0,0 +1,18 @@
+// Lc 1480
+// https://leetcode.com/problems/running-sum-of-1d-array/description/
+
+// Tc O(n)
+// Sc O(1)
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        
+        for(int idx = 1; idx < nums.length; idx++) {
+            ans[idx] = nums[idx] + ans[idx-1];
+        }
+
+        return ans;
+    }
+}
