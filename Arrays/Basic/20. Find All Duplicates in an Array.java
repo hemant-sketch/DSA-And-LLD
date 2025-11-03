6e254f7d829af3232f0bfe488573a7ec8d4a76b3
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-11-03 22:42:46 +0530

    Create 20. Find All Duplicates in an Array.java

---
+++ Arrays/Basic/20. Find All Duplicates in an Array.java
@@ -0,0 +1,26 @@
+// Lc 442
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+// Tc = O(n) 
+// Sc = O(1)
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        int n = nums.length;
+        List<Integer> ans = new ArrayList<>();
+      
+        for(int idx = 0; idx < nums.length; idx++){
+            int original = nums[idx] % (n+1);
+            nums[original - 1] += (n+1);
+        }
+      
+        for(int idx = 0; idx < nums.length; idx++){
+            int freq = nums[idx] / (n+1);
+            if(freq > 1){
+                ans.add(idx+1);
+            }
+        }
+        return ans;
+    }
+}
+
