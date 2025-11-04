4c9a49bf57abcbc5bb42c84586695b9ec92d133e
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-11-04 21:05:39 +0530

    Update 01. Reverse Words in a String.java

--- Strings/01. Reverse Words in a String.java
+++ Strings/01. Reverse Words in a String.java
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/reverse-words-in-a-string/
 
 // Tc = O(N)
-// Sc = O(1)
+// Sc = O(N)
 
 class Solution {
     public String reverseWords(String str) {
@@ -14,5 +14,39 @@ class Solution {
         }
 
         return ans.toString().trim();
+    }
+}
+
+// Tc = O(N)
+// Sc = O(1)
+
+class Solution {
+    public String reverseWords(String s) {
+        int right = s.length()-1;
+        StringBuilder ans = new StringBuilder();
+
+        while(right >= 0) {
+
+            while(right >= 0 && s.charAt(right) == ' ') {
+                right--;
+            }
+            int left = right;
+
+            while(left >= 0 && s.charAt(left) != ' ') {
+                left--;
+            }
+
+            if(ans.length() > 0 && left < right) {
+                ans.append(' ');
+            }
+
+            for(int idx = left + 1; idx <= right; idx++) { 
+                ans.append(s.charAt(idx));
+            }
+
+            right = left;
+        }
+        return ans.toString();
+
     }
 }
