888a77f41ca81f956d24adb633c2ccb3d4d79182
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-11-04 11:19:02 +0530

    Update 23. First Missing Positive.java

--- Arrays/Basic/23. First Missing Positive.java
+++ Arrays/Basic/23. First Missing Positive.java
@@ -15,7 +15,7 @@ class Solution {
 
         for(int idx = 0; idx < n; idx++){
             int original = nums[idx] % (n+1);
-            if(original > 0){
+            if(original > 0){   // original can be -ve because on nums[idx] there could be a negative number
                 nums[original - 1] += (n+1);
             }
         }
@@ -24,6 +24,6 @@ class Solution {
             int freq = nums[idx] / (n+1);
             if(freq == 0) return idx+1;
         }
-        return n+1;
+        return n+1;  //agr koi nahi hai toh n+1 hi hoga first +ve missing number
     }    
 }
