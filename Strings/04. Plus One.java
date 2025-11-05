efc30faf6f9c6980de2a47731a35d5f7456abb29
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-11-05 13:52:18 +0530

    Create 04. Plus One.java

---
+++ Strings/04. Plus One.java
@@ -0,0 +1,22 @@
+// lc 66
+// https://leetcode.com/problems/plus-one/description/
+
+// Tc = O(n)
+// Sc = O(n)
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for(int idx = digits.length-1; idx >= 0 ; idx--) {
+            if(digits[idx] == 9){
+                digits[idx] = 0;
+            }else {
+                digits[idx]++;
+                return digits;
+            }
+        }
+
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        return res; 
+    }
+}
