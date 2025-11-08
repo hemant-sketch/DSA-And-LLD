44a2aa729108692642deb9a30a3f20c807ae2e57
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-11-08 15:54:02 +0530

    Update 02. Climbing Stairs.java

--- Dynamic Programming/02. Climbing Stairs.java
+++ Dynamic Programming/02. Climbing Stairs.java
@@ -33,7 +33,11 @@ class Solution {
     }
 }
 
+
 // memoization
+
+// Tc = O(N)
+// SC = O(N)
 class Solution {
     public int helper(int dp[], int n) {
         if(n == 0) return 1;
@@ -52,6 +56,49 @@ class Solution {
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
         return helper(dp, n);
+    }
+}
+
+
+// tabulation
+
+// Tc = O(N)
+// SC = O(N)
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n == 0) return 0;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+
+        for(int idx = 1; idx <= n; idx++) {
+            int way1 = idx-1 >= 0 ? dp[idx-1] : 0; 
+            int way2 = idx-2 >= 0 ? dp[idx-2] : 0; 
+            dp[idx] = way1 + way2;
+        }
+
+        return dp[n];
+    }
+}
+
+
+// tabulation optimised
+
+// Tc = O(N)
+// SC = O(1)
+class Solution {
+    public int tribonacci(int n) {
+        if(n == 0) return 0;
+        int prev1 = 1;
+        int prev2 = 0;
+
+        for(int idx = 0; idx <= n; idx++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
     }
 }
 
