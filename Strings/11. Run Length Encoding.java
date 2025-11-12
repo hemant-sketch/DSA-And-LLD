6929d862c153778e77eaf1b6685520d1186071fb
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-11-12 11:11:03 +0530

    Create 11. Run Length Encoding.java

---
+++ Strings/11. Run Length Encoding.java
@@ -0,0 +1,30 @@
+// https://www.geeksforgeeks.org/problems/run-length-encoding/1
+
+// Tc = O(N)
+// Sc = O(1)
+
+class Solution {
+    public static String encode(String s) {
+        // code here
+        StringBuilder ans = new StringBuilder();
+        
+        int left = 0; int right = 0;
+        while(left < s.length()) {
+            char ch = s.charAt(left);
+            int freq = 0;
+            
+            while(right < s.length() && s.charAt(right) == ch) {
+                freq++;
+                right++;
+            }
+            
+            ans.append(ch);
+            ans.append(freq);
+            left = right;
+        }
+        
+        return ans.toString();
+        
+        
+    }
+}
