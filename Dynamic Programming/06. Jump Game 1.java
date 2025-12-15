a334974470fbd2ed8f7a48a5f68337810af751fe
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-12-15 23:46:44 +0530

    Implement greedy solution for Jump Game 1

--- Dynamic Programming/06. Jump Game 1.java
+++ Dynamic Programming/06. Jump Game 1.java
@@ -3,3 +3,16 @@
 
 
 . best done using greedy ex for false 2,4,3,1,1,0,4,2
+class Solution {
+    public boolean canJump(int[] jumps) {
+        int reach = 0;
+        for(int idx = 0; idx <  jumps.length; idx++) {
+            if(idx > reach) return false;
+
+            if(jumps[idx] + idx > reach) {
+                reach = jumps[idx] + idx;
+            } 
+        }
+        return true;
+    }
+}
