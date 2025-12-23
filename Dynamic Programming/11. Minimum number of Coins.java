835d8dd84d7d2fd0b4e2a09304c8bec3b9cf72f5
Hemant Chauhan <hemantschauhan042@gmail.com>
2025-12-23 21:27:37 +0530

    Optimize findMin method for minimum coins calculation
    Refactored the findMin method to improve efficiency by reducing the number of iterations needed to calculate the minimum number of coins.

--- Dynamic Programming/11. Minimum number of Coins.java
+++ Dynamic Programming/11. Minimum number of Coins.java
@@ -4,7 +4,6 @@
 // if you are asked about the coins list
 // TC O(N)
 // SC O(N)
-
 class Solution {
     public int findMin(int n) {
         List<Integer> res = new ArrayList<>();
@@ -21,3 +20,46 @@ class Solution {
         return res.size();
     }
 }
+
+// if you are asked about coins number
+// TC O(N)
+// SC O(1)
+class Solution {
+    public int findMin(int n) {
+        int[] coins = {1,2,5,10};
+        int count = 0;
+        
+        for(int idx = coins.length-1; idx >= 0; idx--) {
+            while(n >= coins[idx]) {
+                count++;
+                n -= coins[idx];
+            }
+            if(n == 0) break;
+        }
+        
+        return count;
+    }
+}
+
+// slightly more better
+// TC O(N)
+// SC O(1)
+class Solution {
+    public int findMin(int n) {
+        int[] coins = {1,2,5,10};
+        int count = 0;
+        
+        for(int idx = coins.length-1; idx >= 0; idx--) {
+            count += n/coins[idx];
+            n -= n/coins[idx]*coins[idx];
+            
+            if(n == 0) break;
+        }
+        
+        return count;
+    }
+}
+
+
+
+
