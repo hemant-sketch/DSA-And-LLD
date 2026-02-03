fa6225fe092d555fcbd85140b8f317a2b7b04a1b
Hemant Chauhan <hemantschauhan042@gmail.com>
2026-02-03 22:58:52 +0530

    Update 06. Valid Mountain Array.java

--- Arrays/Basic/06. Valid Mountain Array.java
+++ Arrays/Basic/06. Valid Mountain Array.java
@@ -34,3 +34,30 @@ class Solution {
         return true;
     }
 }
+
+
+// LC 3637 - Trionic Array - 1
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+
+        int idx = 0;
+
+        while(idx + 1 < n && nums[idx] < nums[idx + 1]) {
+            idx++;
+        }
+        if(idx == 0 || idx == n-1) return false;
+
+        while(idx + 1 < n && nums[idx] > nums[idx + 1]) {
+            idx++;
+        }
+        if(idx == n-1) return false;
+
+        while(idx + 1 < n && nums[idx] < nums[idx + 1]) {
+            idx++;
+        }
+
+        return idx == n-1;
+    }
+}
+
