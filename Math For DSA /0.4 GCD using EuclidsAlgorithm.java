54c0e6e19d5d67fa8659a410b95405d6e8e49848
Hemant Chauhan <hemantschauhan042@gmail.com>
2026-05-24 15:46:20 +0530

    Update 0.4 GCD using EuclidsAlgorithm.java

--- Math For DSA /0.4 GCD using EuclidsAlgorithm.java
+++ Math For DSA /0.4 GCD using EuclidsAlgorithm.java
@@ -1,1 +1,31 @@
+// O(log)
 
+class Main {
+
+  // iterative method
+    public static int gcd(int a, int b) {
+        while(a > 0 && b > 0){
+            if(a>b){
+                a = a%b;
+            }else{
+                b = b%a;
+            }
+        }
+        if(a == 0) return b;
+        else return a;
+    }
+
+  // recursive method
+  //assuming a > b
+    public static int gcd(int a, int b) {
+        if(b == 0) return a;
+        
+        return gcd(b, a%b);
+    }
+    
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 28;
+        System.out.println(gcd(6, 12));
+    }
+}
