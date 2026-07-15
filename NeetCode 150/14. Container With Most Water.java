28b6d3b24942321d20e45ec474f2e565a42c6389
Hemant Chauhan <hemantschauhan042@gmail.com>
2026-07-15 16:03:46 +0530

    Create 14. Container With Most Water.java

---
+++ NeetCode 150/14. Container With Most Water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0; 
+        int right = height.length-1;
+        int ans = 0;
+
+        while(left < right){
+            int minHeight = Math.min(height[left], height[right]);
+            ans = Math.max(ans, minHeight * (right - left));
+
+            if(height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return ans;
+    }
+}
