b6dac987123bb8e01b34078feac4899e5a840843
Hemant Chauhan <hemantschauhan042@gmail.com>
2026-07-28 16:31:08 +0530

    Create 28. Binary Search.java

---
+++ NeetCode 150/28. Binary Search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length==1 && nums[0]==target) return 0;
+        int left = 0, right = nums.length-1;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>target){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return -1;
+    }
+}
