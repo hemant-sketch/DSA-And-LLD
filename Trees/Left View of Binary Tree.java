4d76029fa972cadb1eb0467b9172bbf0376bed03
Hemant Chauhan <hemantschauhan042@gmail.com>
2026-08-31 15:53:48 +0530

    Create Left View of Binary Tree.java

---
+++ Trees/Left View of Binary Tree.java
@@ -0,0 +1,27 @@
+// https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+//
+
+class Solution {
+	public ArrayList<Integer> leftView(Node root) {
+		ArrayList<Integer> left = new ArrayList<>();
+		if(root == null) return left;
+		Queue<Node> q = new ArrayDeque<>();
+		
+		q.add(root);
+		
+		while (!q.isEmpty()) {
+			left.add(q.peek().data);
+			int size = q.size();
+			
+			for (int idx = 0; idx < size; idx++) {
+				Node src = q.poll();
+				if (src.left != null)
+					q.add(src.left);
+				if (src.right != null)
+					q.add(src.right);
+			}
+		}
+		
+		return left;
+	}
+}
