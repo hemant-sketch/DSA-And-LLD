b524281a7d04900ada352045734609d917430daa
Hemant Chauhan <hemantschauhan042@gmail.com>
2026-09-08 15:17:15 +0530

    Create 35. N-ary Tree Postorder Traversal.java

---
+++ Trees/35. N-ary Tree Postorder Traversal.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+// TC O(n) SC O(n)
+
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+
+    public void dfs(Node root){
+        for(Node node : root.children){
+            dfs(node);
+        }
+        ans.add(root.val);
+    }  
+    public List<Integer> postorder(Node root) {
+        if(root == null) return ans;
+        dfs(root);
+        return ans;
+    }
+}
